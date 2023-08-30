4402575b61bf30befcf779fb194f1a5f9bb10208
Neha Vishan <56451637+niavish@users.noreply.github.com>
2023-08-30 11:32:31 +0000

    conversionOperstors

---
+++ 01_basics/03_conversionOperstors.js
@@ -0,0 +1,9 @@
+let num1 = 33
+let num2 = "33"
+
+let valueofNumber = Number(num2)
+
+console.log(typeof(num1))
+console.log(typeof(num2))
+console.log(typeof(valueofNumber))
+console.log(valueofNumber)
