e12811f0efd2757758bfc4190256d50aacdb9378
Neha Vishan <56451637+niavish@users.noreply.github.com>
2023-08-30 11:23:06 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //Treat all JS code as newer version 
+
+//alert("hello")  //We are using nodejs , not browser
+
+/* Types of datatpe
+ 1.number 
+ 2. bigint //used for larger number
+ 3. string=""
+ 4. boolen = true/false
+ 5. null = standalone value and aslo a datatype
+ 6. undefined
+ 7. symbole = used to find uniqneness 
+ 
+*/
+
+console.log(typeof "neha")
+console.log(typeof age)
+console.log(typeof null) // object 
+console.log(typeof undefined) //undefined
