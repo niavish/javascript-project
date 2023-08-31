ac1f56e38d2f1fe7425e9e578f5e8f1b4682625d
Neha Vishan <56451637+niavish@users.noreply.github.com>
2023-08-31 10:04:41 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,15 @@
+
+let str ="My name is Neha and am from rohtak"
+
+let newStr = str.replaceAll(" ","") //Remove all white space
+// console.log(str)
+// console.log(newStr) 
+
+let index = 1
+console.log(str.big())
+console.log(str.fontsize(8))
+console.log(newStr.blink())
+console.log(newStr.fontcolor("red"))
+console.log(newStr.lastIndexOf(str))
+
+console.log(`Priting the 7th index ${index} and from the string ${newStr.at(index)}`)  //String interpolation 
