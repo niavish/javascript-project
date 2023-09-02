485a25d75caec82c47a1991ba50c1446bc039f63
Neha Vishan <56451637+niavish@users.noreply.github.com>
2023-09-02 15:09:32 +0000

    func

---
+++ 03_basics/01_function.js
@@ -0,0 +1,38 @@
+
+//functions :
+
+// function display_name () {
+//     console.log("Neha")
+
+// }
+
+// display_name()
+
+/* Display two Numbers */
+// function addToNumber (num1, num2) {
+//     return num1+num2
+
+// }
+// console.log(addToNumber(5,7))
+
+/*login */
+// function loginUserName(userName) {
+//     if(userName ===undefined)
+//     return "User is undefined! Please enter user name"
+// else if (userName === Number)
+//      return "Invaild name! Please valid name"
+// else
+//     return `${userName} just logged in`
+// }
+// console.log(loginUserName())
+
+/* shopping card */
+function calculationCard(...num) {      //speard for dynamic data handling 
+    return num
+}
+
+console.log(calculationCard(10, 20, 30, 40, 50))
+
+
+
+
