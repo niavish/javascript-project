1ae2cfcb746f3ecae6e67d14c90afb2eee01f9cd
Neha Vishan <56451637+niavish@users.noreply.github.com>
2023-09-02 19:18:10 +0000

    java

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -27,12 +27,27 @@
 // console.log(loginUserName())
 
 /* shopping card */
-function calculationCard(...num) {      //speard for dynamic data handling 
-    return num
+// function calculationCard(...num) {      //rest operator for dynamic data handling 
+//     return num
+// }
+
+// console.log(calculationCard(10, 20, 30, 40, 50))
+
+//Exmaple of rest operator :
+// function restExample(val1, val2, ...num) {
+//     return num
+// }
+// console.log(restExample(10,20,30,40,50))
+
+
+//How to pass object to a function :
+
+
+const customer = {
+    name : "Customer1",
+    age : 36
 }
-
-console.log(calculationCard(10, 20, 30, 40, 50))
-
-
-
-
+function displayCustomer(anyobject) {
+    console.log(`Customer Name :${anyobject.name} and Age : ${anyobject.age}`)
+}
+displayCustomer(customer)
