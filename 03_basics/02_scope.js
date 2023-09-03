f1212664ca6ab40c9c1a59b80c6a1d86c6ecbc0b
Neha Vishan <56451637+niavish@users.noreply.github.com>
2023-09-03 09:15:26 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,19 @@
+/* SCOPE
+
+{} is called scope
+
+
+*/
+
+// let a= 10
+// const b = 20
+// var c=30
+
+/* Block scope */
+if (true){
+    let a= 60
+   const b = 70
+   var c=80
+} 
+console.log(` b:${b} c:${c}`)
+
