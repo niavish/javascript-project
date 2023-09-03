7f091808ec8722f3913fa844cdee00c2ba35e84e
Neha Vishan <56451637+niavish@users.noreply.github.com>
2023-09-03 12:27:03 +0000

    interview

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,12 @@
+
+//IIFE : Immedialty invoked function expression
+
+// 1. Named IIFE
+(function day() {
+     console.log(`I am named IIFE`)
+})();
+
+//2. IIFE
+(() => {
+     console.log("Un-named IIFE")
+})()
