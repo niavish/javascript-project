7848ad506594715eb78505d49d9f258499e99100
Neha Vishan <56451637+niavish@users.noreply.github.com>
2023-09-03 10:49:59 +0000

    interview

--- 03_basics/03_arrow_&_this.js
+++ 03_basics/03_arrow_&_this.js
@@ -1,11 +1,41 @@
 /***this() */
+
+
 const user = {
     username : "Neha",
     price : 9999,
 
     welcomeMessage: function() {
-        console.log(`${this.username} and Price :${this.price}`)
+        console.log(`Welcome ${this.username} and Price :${this.price}`)
     }
 }
+// user.username = "XYZZZZZZz"
+// console.log(user.welcomeMessage())        //Welcome Neha and Price :9999
+// console.log(user.welcomeMessage(this))    // Welcome Neha and Price :9999
 
-user.welcomeMessage
+//console.log(this)   //{}
+
+const youtube = () => {
+    let username= "Neha"
+    console.log(this.username)
+}
+//youtube()
+
+
+/***Arrow function */
+
+// const add = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// console.log(add(3,8))
+
+
+/*** Implicit retrun */
+const addTwo = (num1, num2) => (num1 +num2)
+console.log(addTwo(7,2))
+
+
+/**Impilict way to return object */
+const display = (username) => ( {name : "Neha" })
+console.log(display("Vishan"))
