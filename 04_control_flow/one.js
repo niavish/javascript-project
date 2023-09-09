0d5402ad2d6aab04607a3b8d63f3614d1dfed3ee
Neha Vishan <56451637+niavish@users.noreply.github.com>
2023-09-09 09:10:24 +0000

    commit

---
+++ 04_control_flow/one.js
@@ -0,0 +1,13 @@
+//
+
+// if (3 === "3")
+// console.log("executed")
+
+const temperature = 40
+
+if (temperature > 50) 
+
+    console.log("Temperature is less than 50 ")
+ else
+
+console.log("Temparature is greater than 40")
