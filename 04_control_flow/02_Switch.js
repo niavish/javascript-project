7f48853ab2033c7cc915dea70c550bc75560c95e
Neha Vishan <56451637+niavish@users.noreply.github.com>
2023-09-09 09:24:23 +0000

    commi

---
+++ 04_control_flow/02_Switch.js
@@ -0,0 +1,19 @@
+///Switch statement 
+
+const month = 4
+
+switch(month) {
+    case 1 :
+        console.log("Jan")
+        break;
+    case 2 :
+        console.log("feb")
+        break;
+    case 3 :
+        console.log("March")
+        break;
+    default:
+        console.log("Invalid month!")
+        break;
+  
+}
