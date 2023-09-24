6716324fe6222613d159934b9f46060450d29f49
Neha Vishan <56451637+niavish@users.noreply.github.com>
2023-09-24 10:34:29 +0000

    while

---
+++ 04_control_flow/03_while_loop.js
@@ -0,0 +1,21 @@
+/*
+We have 2 while loops 
+ 1. While
+ 2. Do while
+*/
+
+//While loop
+let myArr = [1,2,3,4,5]
+let arr =0
+ while (arr <myArr.length)  {
+    console.log(myArr[arr])
+    arr++;
+ }
+
+ //Do while 
+ let score = [10,20,30,40]
+ let num = 11
+do {
+    console.log(score)
+    score --
+} while(num > score.length)
