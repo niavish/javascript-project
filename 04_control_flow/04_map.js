bf07b8522ce77d104ed142ef63923c545f84c7c6
Neha Vishan <56451637+niavish@users.noreply.github.com>
2023-09-24 16:13:57 +0000

    commit

---
+++ 04_control_flow/04_map.js
@@ -0,0 +1,13 @@
+// MAP : contains unique values 
+// Key , value pair
+
+const map = new Map()
+map.set(1, 'Neha')
+map.set(2, 'Alok')
+map.set(3, 'Suraj')
+map.set(4, 'Siya')
+
+for (const [key, value] of map) {
+    console.log(key)
+    
+}
---
+++ 04_control_flow/05_for_0f_loop.js
@@ -0,0 +1,15 @@
+/// "FOR OF" loop
+
+let arr = [1,2,3,4,5,6]
+
+for (const num of arr) {
+    console.log(num)   
+}
+
+//Strings
+
+let greetings ="Hello World"
+
+for (const greet of greetings) {
+    console.log(greet)
+}
